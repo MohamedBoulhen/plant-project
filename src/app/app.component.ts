1648599f71f489fc33aefff3f8f1a9aa57e3f563
MohamedBoulhen <151053412+MohamedBoulhen@users.noreply.github.com>
2024-08-25 14:08:17 +0200

    Save changes before updating Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'] // Correct property name
 })
 export class AppComponent {
   title = 'plant-project';
